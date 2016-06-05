b66c510c5c47ec7fa188af03fb06f5fd5a52c588
Oriolowo Nancy <oriolowonancy@gmail.com>
2016-06-06 00:30:35 +0100

    nancee commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diffvarconvrsns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt;
+            double myDouble;
+            string myString;
+
+            myInt = 20;
+
+            myDouble = myInt;
+            Console.WriteLine("myDouble is {0}.", myDouble);
+
+            myDouble = 10.5;
+            myInt = myDouble; //a double can't be converted to int(not without casting)
+            Console.WriteLine(myInt);
+
+            myInt = (int)myDouble; //i could write it as: myInt = (int)10.5
+            Console.WriteLine("After casting, myInt = {0}.", myInt);
+
+            myString = myDouble.ToString();
+            Console.WriteLine("myDouble as a string: {0}", myString);
+
+            Console.Write("Please enter a number: ");
+            myString = Console.ReadLine();
+
+            Console.WriteLine("myString converted to double: {0}", Convert.ToDouble(myString));
+            Console.ReadLine();
+
+        }
+    }
+}
